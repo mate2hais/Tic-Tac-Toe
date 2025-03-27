0190c51d8d8d1759052dfbb4f665829d6693db66
mate2hais <matei.adrian694@yahoo.com>
2025-03-27 13:50:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let playerOne = document.getElementById('Player1');
-let playerTwo = document.getElementById('Player2');
 tableAudio = document.createElement('audio');
 tableAudio.src = 'http://www.freesound.org/data/previews/265/265296_5003039-lq.mp3'; tableAudio.volume=0.5;
+let xCount = 0, zeroCount = 0;
+const xBtn = document.getElementById('xButton').addEventListener('click', xSide);
+const zeroBtn = document.getElementById('0Button').addEventListener('click', zeroSide);
+
+function xSide() {
+  tableAudio.currentTime=0.09; tableAudio.play();
+  ++xCount;
+}
+
+function zeroSide() {
+  tableAudio.currentTime=0.09; tableAudio.play();
+  ++zeroCount;
+}
 
 const butonSign = document.getElementById('signChoose').addEventListener("click", chooseSign)
 function chooseSign () {
   tableAudio.currentTime=0.09; tableAudio.play();
-  inputSign = document.getElementById('sign').value;
-  if (inputSign == "x") {
-    document.getElementById('Player1').innerHTML = "Player 1 choose " +  "|" + inputSign + "|" + " sign";
+  if (xCount) {
+    document.getElementById('Player1').innerHTML = "Player 1 choose |x| sign";
     document.getElementById('Player2').innerHTML = "Player 2 have |0| sign";
-    playerOne = inputSign;
+    playerOne =  "x";
     playerTwo = "0";
-  } else if (inputSign == "0") {
-    document.getElementById('Player1').innerHTML = "Player 1 choose " + "|" + inputSign + "|" + " sign";
+  } else if (zeroCount) {
+    document.getElementById('Player1').innerHTML = "Player 1 choose |0| sign";
     document.getElementById('Player2').innerHTML = "Player 2 have |x| sign";
-    playerOne = inputSign;
+    playerOne = "0";
     playerTwo = "x";
   }
 }
@@ -29,9 +39,9 @@ for (let i = 0; i < rows; ++i) {
     my2DArray[i][j] = "_";
   }
 }
-
 let line = 0;
 let col = 0;
+
 let setTable = document.getElementById('table').addEventListener("click", insert)
 function insert() {
   for (let i = 0; i < table.rows.length; ++i) { 
@@ -70,7 +80,7 @@ function getVal(cell) {
 let draw = 0;
 function verify() {
   ++draw; 
-  if (my2DArray[0][0] == playerOne &&  my2DArray[0][1] == playerOne &&  my2DArray[0][2] == playerOne) {
+  if (my2DArray[0][0] ==  my2DArray[0][1]  &&  my2DArray[0][1] == my2DArray[0][2]) {
     return 1;
   } else if (my2DArray[0][0] == playerTwo &&  my2DArray[0][1] == playerTwo &&  my2DArray[0][2] == playerTwo) {
     return 2;
@@ -129,7 +139,7 @@ function restartGame(){
       table.rows[i].cells[j].innerHTML = ''  ;
     }
   }
-  draw = 0;
+  draw = 0, xCount = 0, zeroCount = 0;
   document.getElementById('sign').value = "";
   document.getElementById('Player1').innerHTML = ".";
   document.getElementById('Player2').innerHTML  = ".";
